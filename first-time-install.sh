c55b46a9d83c2e74f9edcc44426cd4c05f384825
sebashack <sebastian0092@gmail.com>
2021-05-09 14:45:20 -0500

    Tweak first-time-install

--- first-time-install.sh
+++ first-time-install.sh
@@ -4,4 +4,5 @@ set -xeuf -o pipefail
 
 # Install system deps
 sudo apt-get update
+sudo apt install libqt5printsupport5 libqt5xml5 libqt5sql5 libqt5serialport5 libqt5sql5-sqlite
 sudo apt-get install -y fritzing fritzing-data fritzing-parts
